ab89b84f11742d5f477ad8e9d56f266782008a79
Vladimir Tomanovski <vladoohr@gmail.com>
2016-12-20 21:40:04 +0100

    Set mail settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'advertisementsserver.herokuapp.com' }
+
+  # Set SMPT
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :domain               => "gmail.com",
+   :authentication       => "plain",
+   :enable_starttls_auto => true,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+  }
 end
